f449b2e851dbff1ed19f62918c614a5a548bd078
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-22 05:04:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(array) {
+  return array.map(function(driver) {
+    return driver.toLowercase()
+  })
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    const fn = driver.split(' ')[0];
+    const ln = driver.split(' ')[1];
+    return { firstName: fn, lastName: ln }
+  })
+}
+
+function attributesToPhrase(array) {
+  return array.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
