18c97dfe7c923dc1916aab0870d24ebdad204e4e
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-08 01:56:17 +0000

    commit tree 0a3b86008f14954ab9e510a3fa8343bdff0f6808

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 // Code your solution in this file.
 
 function lowerCaseDrivers(array) {
-  return array.map(function(driver) {
-    return driver.toLowercase()
-  })
+  return array.map(e => e.toLowerCase())
 }
 
 function nameToAttributes(drivers) {
-  return drivers.map(function(driver) {
-    const fn = driver.split(' ')[0];
-    const ln = driver.split(' ')[1];
-    return { firstName: fn, lastName: ln }
+
+  return drivers.map(function(e) {
+    let arr = e.split(" ");
+    
+    return { firstName: arr[0], lastName: arr[1] };
   })
 }
 
 function attributesToPhrase(array) {
-  return array.map(function(driver) {
-    return `${driver.name} is from ${driver.hometown}`;
-  });
+  
 }
